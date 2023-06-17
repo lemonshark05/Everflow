ca789a363aabdfcceeb1f19a4cde577abdd78feb
zhimin meng <mengzhimin5@gmail.com>
2023-06-17 12:59:23 -0700

    init

---
+++ Everflow/EverflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverflowApp.swift
+//  Everflow
+//
+//  Created by lemonshark on 2023/6/17.
+//
+
+import SwiftUI
+
+@main
+struct EverflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
